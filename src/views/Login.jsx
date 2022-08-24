a924c4926713d4f2bf33819eae4ffc9b88fba179
Hank-Ad2-MBP16 <cgh20xx@gmail.com>
2022-08-24 18:00:06 +0800

    feat: 新增送出表單欄位驗証功能

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,3 +1,42 @@
+import { useForm } from 'react-hook-form';
+
 export const Login = () => {
-  return <p>這是登入頁面</p>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  return (
+    <div>
+      <h1>這是登入頁</h1>
+      <div>
+        <label>
+          帳號:
+          <input
+            {...register('email', {
+              required: { value: true, message: '帳號未填' },
+            })}
+          />
+          {errors.email?.message}
+        </label>
+      </div>
+      <div>
+        <label>
+          密碼:
+          <input
+            type="password"
+            {...register('password', {
+              required: { value: true, message: '密碼未填' },
+            })}
+          />
+        </label>
+        {errors.password?.message}
+      </div>
+      <button onClick={handleSubmit(onSubmit)}>登入</button>
+    </div>
+  );
 };
